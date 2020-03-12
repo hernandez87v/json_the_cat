1030231f2bd1f056aed05a9e47972b30ba615612
Vlad <hernandez87v@hotmail.com>
2020-03-12 22:59:27 +0000

    Adding error, undefined, description outputs

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,10 +1,16 @@
 const request = require('request');
 const uploadCom = process.argv.slice(2);
-const search = 'https://api.thecatapi.com/v1/breeds/search/?q=';
+const search = 'https://api.hecatapi.com/v1/breeds/search/?q=';
 
 let find = search + uploadCom;
 
 request(find, function(error, response, body) {
   const data = JSON.parse(body);
-  console.log(data[0].description);
+  if (error) {
+    return error;
+  } else if (data[0] === undefined) {
+    console.log('Not found');
+  } else {
+    console.log(data[0].description);
+  }
 });
